5456e311dec9502c49cfde98a00f7ff43ae017e9
RaDix69 <ronal4456@gmail.com>
2021-11-05 11:43:36 -0500

    a

--- crudProductos/indexmodificar.php
+++ crudProductos/indexmodificar.php
@@ -25,44 +25,70 @@ return[
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mvc.css">
+    <link rel="stylesheet" href="estilos/estiloIngresar.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;1,600&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="img/logo.png" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300&display=swap" rel="stylesheet">
     <title>Actualizar Datos Productos</title>
-    <link rel="stylesheet" href="./estilos/estiloActualizar.css">
+    <!-- <link rel="stylesheet" href="./estilos/estiloActualizar.css"> -->
 </head>
 <body>
-    <form action="controlador/controlmodificar.php" method="post" class="formulario">
+    <div class="contenedor">
+        <section>
+            <div class="formulario">
+                <form action="controlador/controlmodificar.php" method="post" class="formulario">
     <div class="linea">
     <h1>Actualizar datos del Producto</h1>
-    <label for="">Id_producto: </label>    
-    <input required="" class="entrada" type="text" name="id_producto" id="id_producto" class="form-control" value="<?php echo $consulta[0]?>" style="display:none;">
+    <label for="">Id_producto: </label>
+    <div class="tooltip">
+<span class="tooltiptext">Sube la imágen del producto acá</span>
+<input required="" class="entrada" type="text" name="id_producto" id="id_producto" class="form-control" value="<?php echo $consulta[0]?>" style="display:none;">
     <input required="" class="entrada" type="text" name="id_producto2" id="id_producto2" class="form-control" value="<?php echo $consulta[0]?>" disabled>
     <br><br>
-    <label for="">Proveedor: </label>    
-    <input required="" class="entrada" type="text" name="proveedor" id="proveedor" class="form-control" pattern="[A-Za-z0-9 ]{1,100}" value="<?php echo $consulta[1]?>">
+</div>    
+    
+    <label for="">Proveedor: </label>   
+    <div class="tooltip">
+<span class="tooltiptext">Sube la imágen del producto acá</span>
+<input required="" class="entrada" type="text" name="proveedor" id="proveedor" class="form-control" pattern="[A-Za-z0-9 ]{1,100}" value="<?php echo $consulta[1]?>">
     <br><br>
+</div> 
+    
     <label for="">Nombre producto: </label>    
-    <input required="" class="entrada" type="text" name="nombrep" id="nombrep" class="form-control" pattern="[A-Za-z0-9 ]{1,100}" value="<?php echo $consulta[2]?>">
+    <div class="tooltip">
+<span class="tooltiptext">Sube la imágen del producto acá</span>
+<input required="" class="entrada" type="text" name="nombrep" id="nombrep" class="form-control" pattern="[A-Za-z0-9 ]{1,100}" value="<?php echo $consulta[2]?>">
     <br><br>
+</div>
+    
     <label for="">Id_categoria: </label>    
-    <input required="" class="entrada" type="number" name="id_categoria" id="id_categoria" class="form-control" value="<?php echo $consulta[3]?>" style="display:none;">
+    <div class="tooltip">
+<span class="tooltiptext">Sube la imágen del producto acá</span>
+<input required="" class="entrada" type="number" name="id_categoria" id="id_categoria" class="form-control" value="<?php echo $consulta[3]?>" style="display:none;">
     <input required="" class="entrada" type="number" name="id_categoria2" id="id_categoria2" class="form-control" value="<?php echo $consulta[3]?>" disabled >
     <br><br>
-    <label for="">Descripcion: </label>    
-    <textarea required="" class="entrada" name="descripcion" id="descripcion" class="form-control" value="" pattern="[A-Za-z0-9 ]{1,100}" minlength="10" maxlength="200"><?php echo $consulta[4]?></textarea>   
+</div>
+    
+    <label for="">Descripcion: </label>   
+    <div class="tooltip">
+<span class="tooltiptext">Sube la imágen del producto acá</span>
+<textarea required="" class="entrada" name="descripcion" id="descripcion" class="form-control" value="" pattern="[A-Za-z0-9 ]{1,100}" minlength="10" maxlength="200"><?php echo $consulta[4]?></textarea>   
     <br><br>
+</div> 
+    
     <label for="">Precio: </label>    
+    
     <input required="" class="entrada" type="number" name="precio" id="precio" class="form-control" value="<?php echo $consulta[5]?>">
     <br><br>
 
-    <input type="submit" name="boton" value="Actualizar Producto" class="btn btn-primary" >
-    <input type="submit" name="boton" value="Regresar" class="btn btn-primary" >
+    <input type="submit" name="boton" value="Actualizar Producto" class="boton" >
+    <input type="submit" name="boton" value="Regresar" class="boton" >
 
     </form>
+            </div>
+        </section>
+    </div>
+    
     
     </div> 
 </div>    
